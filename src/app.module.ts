6b307ac5715757f752eb0bbeebf44cdfb9b4b914
ziyu-0123 <ziyuliu123@qq.com>
2026-08-29 14:24:42 +0800

    feat: connect MongoDB and create schema models

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { QuestionModule } from './question/question.module.js';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [QuestionModule],
+  imports: [
+    QuestionModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get<string>('MONGO_HOST')}:${configService.get<string>('MONGO_PORT')}/${configService.get<string>('MONGO_DATABASE')}`,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { QuestionController } from './question.controller.js';
 import { QuestionService } from './question.service.js';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Question, QuestionSchema } from './schemas/question.schema.js';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: Question.name, schema: QuestionSchema}])],
   controllers: [QuestionController],
   providers: [QuestionService]
 })
-export class QuestionModule {}
+export class QuestionModule { }
