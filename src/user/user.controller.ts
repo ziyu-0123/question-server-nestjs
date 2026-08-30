1f76c9b6daeaee95ca90cff2ff2bb9021a848c4a
ziyu-0123 <ziyuliu123@qq.com>
2026-08-30 16:40:04 +0800

    feat: add redirect to user routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Body, Post, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Get,
+  Redirect,
+  Post,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { UserService } from './user.service.js';
 import { CreateUserDto } from './dto/create-user.dto.js';
+import { Public } from '../auth/decorators/public.decorator.js';
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
 
+  @Public()
   @Post('register')
   async register(@Body() userDto: CreateUserDto) {
     try {
@@ -15,5 +25,18 @@ export class UserController {
         HttpStatus.BAD_REQUEST,
       );
     }
+  }
+
+  @Get('info')
+  @Redirect('/api/auth/profile', 302) //http状态码302表示临时定向，301表示永久定向(get方法)
+  async info() {
+    return;
+  }
+
+  @Public()
+  @Post('login')
+  @Redirect('/api/auth/login', 307) //http状态码307表示临时定向，308表示永久定向(post方法)
+  async login() {
+    return;
   }
 }
