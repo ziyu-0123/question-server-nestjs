fda7eba96cdec07cf17ca40d2b5eab30c460ad68
ziyu-0123 <ziyuliu123@qq.com>
2026-08-31 14:31:18 +0800

    feat: add statistics for single questionnaire

--- src/stat/stat.controller.ts
+++ src/stat/stat.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { StatService } from './stat.service.js'
 @Controller('stat')
-export class StatController {}
+export class StatController {
+  constructor(private readonly statService: StatService) { }
+
+  @Get(':questionId')
+  async getQuestionStat(@Param('questionId') questionId: string, @Query('page') page: number = 1, @Query('pageSize') pageSize: number = 10) {
+
+    return this.statService.getQuestionStatListAndCount(questionId, { page, pageSize })
+  }
+}
--- src/stat/stat.service.ts
+++ src/stat/stat.service.ts
@@ -1,4 +1,104 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { QuestionService } from '../question/question.service.js'
+import { AnswerService } from '../answer/answer.service.js'
+type OptionItem = {
+  value: string
+  text: string
+}
+
+type ComponentProps = {
+  options?: OptionItem[]
+  list?: (OptionItem & { checked?: boolean })[]
+}
+
+type ComponentInfo = {
+  fe_id: string
+  type: string
+  title: string
+  props: ComponentProps
+}
+
+type QuestionLike = {
+  componentList?: ComponentInfo[]
+}
+
+type AnswerItem = {
+  componentId: string
+  value: string
+}
 
 @Injectable()
-export class StatService {}
+export class StatService {
+  constructor(private readonly questionService: QuestionService, private readonly answerService: AnswerService) { }
+
+  private _getRadioOptText(value: string, props: ComponentProps = {}): string {
+    const { options = [] } = props
+    for (const item of options) {
+      if (item.value === value) {
+        return item.text
+      }
+    }
+    return ''
+  }
+
+  private _getCheckboxOptText(value: string, props: ComponentProps = {}): string {
+    const { list = [] } = props
+    for (const item of list) {
+      if (item.value === value) {
+        return item.text
+      }
+    }
+    return ''
+  }
+
+  private _genAnswersInfo(
+    question: QuestionLike,
+    answerList: AnswerItem[] = []
+  ): Record<string, string> {
+    const res: Record<string, string> = {}
+    const { componentList = [] } = question
+
+    answerList.forEach((a) => {
+      const { componentId, value = '' } = a
+      const comp = componentList.find((c) => c.fe_id === componentId)
+      if (!comp) return
+
+      const { type, props = {} } = comp
+      if (type === 'questionRadio') {
+        res[componentId] = this._getRadioOptText(value, props)
+      } else if (type === 'questionCheckbox') {
+        // 多选值在 C 端以逗号拼接，这里拆开逐项取文案
+        const vals = value ? value.split(',') : []
+        res[componentId] = vals
+          .map((v) => this._getCheckboxOptText(v, props))
+          .filter(Boolean)
+          .toString()
+      } else {
+        res[componentId] = value
+      }
+    })
+
+    return res
+  }
+
+  async getQuestionStatListAndCount(questionId: string, opt: { page: number, pageSize: number }) {
+    const noData = { list: [], total: 0 }
+    if (!questionId) {
+      return noData
+    }
+    const q = await this.questionService.findOne(questionId)
+    if (q == null) {
+      return noData
+    }
+    const total = await this.answerService.count(questionId)
+    if (total === 0) return noData
+    const answers = await this.answerService.findAll(questionId, opt)
+    const list = answers.map((a) => {
+      return {
+        _id: a._id,
+        ...this._genAnswersInfo(q, a.answerList)
+      }
+    })
+    return { list, total }
+  }
+}
